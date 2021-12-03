325579ffe9b62b2e0c5efc0b1235180d5ef8f1ad
Ostap Maliuvanchuk <ostap@salemove.com>
2021-12-03 10:27:58 +0200

    Day 3 and template

--- 2/code.exs
+++ 2/code.exs
@@ -1,38 +1,80 @@
-lines = "input.txt" |> File.read!() |> String.split("\n") |> Enum.filter(& &1 != "")
+defmodule Solution do
+  def part1(input) do
+    lines = parse_input(input)
 
-defmodule Helpers do
-  def int(str) do
-    {num, _} = Integer.parse(str)
-    num
+    {h, v} =
+      lines
+      |> Enum.reduce({0, 0}, fn
+        "forward " <> steps, {h, v} ->
+          {h + String.to_integer(steps), v}
+        "down " <> steps, {h, v} ->
+          {h, v + String.to_integer(steps)}
+        "up " <> steps, {h, v} ->
+          {h, v - String.to_integer(steps)}
+      end)
+
+    h * v
+  end
+
+  def part2(input) do
+    lines = parse_input(input)
+
+    {h, v, _} =
+      lines
+      |> Enum.reduce({0, 0, 0}, fn
+        "forward " <> steps, {h, v, a} ->
+          {h + String.to_integer(steps), v + a * String.to_integer(steps), a}
+        "down " <> steps, {h, v, a} ->
+          {h, v, a + String.to_integer(steps)}
+        "up " <> steps, {h, v, a} ->
+          {h, v, a - String.to_integer(steps)}
+      end)
+
+    h * v
+  end
+
+  def parse_input(input) do
+    input |> String.split("\n") |> Enum.filter(& &1 != "")
   end
 end
 
+defmodule Test do
+  def assert(actual, expected) do
+    if actual == expected do
+      IO.puts("Passed!")
+    else
+      IO.puts("Expected #{expected}, actual #{actual}")
+    end
+  end
 
-# Part 1
-{h, v} =
-  lines
-  |> Enum.reduce({0, 0}, fn
-    "forward " <> steps, {h, v} ->
-      {h + Helpers.int(steps), v}
-    "down " <> steps, {h, v} ->
-      {h, v + Helpers.int(steps)}
-    "up " <> steps, {h, v} ->
-      {h, v - Helpers.int(steps)}
-  end)
+  def part1 do
+    """
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2
+    """
+    |> Solution.part1()
+    |> assert(150)
+  end
 
-IO.inspect(h * v)
+  def part2 do
+    """
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2
+    """
+    |> Solution.part2()
+    |> assert(900)
+  end
+end
 
-
-# Part 2
-{h, v, _} =
-  lines
-  |> Enum.reduce({0, 0, 0}, fn
-    "forward " <> steps, {h, v, a} ->
-      {h + Helpers.int(steps), v + a * Helpers.int(steps), a}
-    "down " <> steps, {h, v, a} ->
-      {h, v, a + Helpers.int(steps)}
-    "up " <> steps, {h, v, a} ->
-      {h, v, a - Helpers.int(steps)}
-  end)
-
-IO.inspect(h * v)
+Test.part1()
+Test.part2()
+IO.inspect(Solution.part1(File.read!("input.txt")), label: "Part 1")
+IO.inspect(Solution.part2(File.read!("input.txt")), label: "Part 2")
